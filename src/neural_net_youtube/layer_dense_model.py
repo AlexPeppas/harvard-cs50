b149508210192d502abb3ef7ba85841978d13c71
Alexandros Peppas <apeppas@microsoft.com>
2025-12-16 11:26:10 +0000

    implement categorical cross entropy in final project and calcluate loss of my small handmade N layer neural net

--- src/neural_net_youtube/layer_dense_model.py
+++ src/neural_net_youtube/layer_dense_model.py
@@ -38,6 +38,32 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis = 1, keepdims = True)
         return probabilities
 
+class Loss:
+    def calculate(self, layer_outputs, y) -> int:
+        batch_loss = self.forward(layer_outputs, y)
+        mean_batch_loss = np.mean(batch_loss)
+        return mean_batch_loss
+
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_predicted , y_true):
+        samples = len(y_predicted)
+        # clip will put upper and lower boundaries in the layer output predicted values
+        # that's in order to guard against a confident prediction of 0 because np.log(0) will return infinite.
+        # Thus in the Loss.Calculate it will cause np.mean to always return infinite. 
+        # 1e-7 is very close to 0 and 1-1e-7 is very close to 1.
+        y_predicted_clipped = np.clip(y_predicted, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1: # the expected true values are in scalar shape
+            correct_confidences = y_predicted_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2: # the expected true values are in one-hot-encoded vectors
+            correct_confidences = np.sum(y_predicted_clipped * y_true, axis = 1)
+        else:
+            raise Exception(f'predicted values shape is not supported. Len:{len(y_true)}')
+        
+        batch_loss = -np.log(correct_confidences)
+        return batch_loss
+    
+
 layer1 = Layer_Dense(2, 3)
 layer2 = Layer_Dense(3, 3)
 #layer3 = Layer_Dense(7,2)
@@ -47,3 +73,10 @@ layer2.forward(layer1.outputs, algo="softmax")
 #layer3.forward(layer2.outputs)
 
 print(layer2.outputs[:5])
+
+cce_loss = Loss_CategoricalCrossEntropy()
+
+# calcualte the loss of last layer's output against the y true values 
+neural_net_loss = cce_loss.calculate(layer2.outputs, y)
+
+print (f'Overall mean loss: {neural_net_loss}')
